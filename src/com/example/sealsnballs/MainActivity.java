ce0d9f20981c0a93341452529baef7152b7c7a8e
Meber5 <meberle@exeter.edu>
2016-02-19 09:31:40 -0500

    second change for mike

--- src/com/example/sealsnballs/MainActivity.java
+++ src/com/example/sealsnballs/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 //First change (Github test)
-//change by Mike Eberle
+//change by Mike Eberle2
 public class MainActivity extends Activity {
 
 	@Override
