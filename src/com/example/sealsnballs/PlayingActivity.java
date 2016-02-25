3c9b0f74d5efdbe2197b5161773922fda27e77da
tjeanjacques <tjeanjacques@10.2.12.107>
2016-02-25 09:06:45 -0500

    taylor changes

--- src/com/example/sealsnballs/PlayingActivity.java
+++ src/com/example/sealsnballs/PlayingActivity.java
@@ -1,21 +1,15 @@
 package com.example.sealsnballs;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends Activity {
+public class PlayingActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.playingactivity);
 	}
-	public void onClick(View view){
-		startActivity(new Intent("com.example.PlayingActivity"));
-	}
-
 }
