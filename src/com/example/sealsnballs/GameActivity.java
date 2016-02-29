4988b9a83ccfd4f6c9030167c9bb2f92eefd113e
Meber5 <meberle@exeter.edu>
2016-02-29 16:00:13 -0500

    fixed genseals maybe

--- src/com/example/sealsnballs/GameActivity.java
+++ src/com/example/sealsnballs/GameActivity.java
@@ -4,6 +4,7 @@ package com.example.sealsnballs;
 	
 	
 	import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Activity;
 import android.graphics.Color;
@@ -24,7 +25,23 @@ import android.os.Bundle;
 		        genSeals seals = new genSeals();
 		        Timer timer = new Timer();
 		        
-		        timer.scheduleAtFixedRate(new genSeals(), 500,500);
+		        timer.scheduleAtFixedRate(new TimerTask(){
+		        	public void run(){
+		        		Seal seal= new Seal(render.canvas.getWidth(),800,1,false);
+		        		render.seals.add(seal);
+		        		
+		        		for(int i=0;i<render.seals.size();i++){
+		        			Seal s = render.seals.get(i);
+		        			int curX = s.getX();
+		        			curX -= 5;
+		        			s.setX(curX);
+		        			
+		        			render.invalidate();
+		        		}
+		        	}
+		        	
+		        }, 
+		        		500,500);
 		    
 		    }    
 }
--- src/com/example/sealsnballs/genSeals.java
+++ src/com/example/sealsnballs/genSeals.java
@@ -5,13 +5,12 @@ import java.util.TimerTask;
 import android.content.Context;
 
 public class genSeals extends TimerTask {
-	Context context = new Context();
+	
 	GameRender render = new GameRender(context);
 	int length = render.seals.size();
 	
 	public void run(){
-		int count = 0;
-		count++;
+		
 		
 		Seal seal= new Seal(render.canvas.getWidth(),800,1,false);
 		render.seals.add(seal);
@@ -20,6 +19,7 @@ public class genSeals extends TimerTask {
 			Seal s = render.seals.get(i);
 			int curX = s.getX();
 			curX -= 5;
+			s.setX(curX);
 			
 			render.invalidate();
 			
